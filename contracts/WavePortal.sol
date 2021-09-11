e4cbc9fa74f3b2bc4638162fd11d66589ff899dc
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-11 18:24:41 -0300

    Start counting waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+	uint256 totalWaves;
+
 	constructor() {
 		console.log("Hey fam, I am a contract and I am smart!");
+	}
+
+	function wave() public {
+		totalWaves += 1;
+		console.log("%s is waved!", msg.sender);
+	}
+
+	function getTotalWaves() public view returns (uint256) {
+		console.log("We have %d total waves", totalWaves);
+		return totalWaves;
 	}
 }
